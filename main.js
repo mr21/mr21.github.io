30fb3bafe4d35825c740a2ba252937f5e040ec7b
Mr21 <thomastortorini@gmail.com>
2015-02-10 22:19:58 +0100

    JS/CSS, toggle works (need clean)

--- main.js
+++ main.js
@@ -9,9 +9,14 @@ $(function() {
 	document.body.lang = "en";
 
 	$.each(window.projectsData, function() {
-		$('<a href="#' + this.href + '" style="background-image: url(' + this.img + ');"></a>')
-			.appendTo(jq_projectsContent)
-			[0].pageData = this;
+		var a = $('<a style="background-image: url(' + this.img + ');"></a>')
+			.prop("pageData", this)
+			.attr({
+				href:     "#" + this.href,
+				hrefOld:  "#" + this.href,
+				style:    "background-image: url('" + this.img + "');"
+			})
+			.appendTo(jq_projectsContent);
 	});
 
 	$(".rub .content").each(function() {
@@ -30,12 +35,14 @@ $(function() {
 	});
 
 	function writeData(jq_a) {
-		var d = jq_a[0].pageData;
-		if (jq_aOld)
-			jq_aOld
-				.removeClass("selected");
+		var d = jq_a.prop("pageData");
+		if (jq_aOld) {
+			jq_aOld.attr("href", jq_aOld.attr("hrefOld"));
+		}
 		jq_a
-			.addClass("selected");
+			.attr("href", "#")
+			.parent()
+				.addClass("selected");
 		jq_page
 			.children("h2").text(d.title).end()
 			.children("a").prop("href", d.href).end()
@@ -44,38 +51,58 @@ $(function() {
 		jq_aOld = jq_a;
 	}
 
-	function setPage(jq_a) {
-		writeData(jq_a);
-	}
-
 	window.onhashchange = function() {
-		var jq_a = $("[href='" + window.location.hash + "']");
-		if (jq_a[0] && jq_a[0].pageData) {
-			var	jq_aNext_before,
-				jq_aNext = jq_a;
-			for (;;) {
-				jq_aNext_before = jq_aNext;
-				jq_aNext = jq_aNext.next();
-				if (!jq_aNext[0])
-					jq_aNext = jq_aNext_before;
-				else if (jq_aNext[0].tagName !== "A")
-					jq_aNext = null;
-				else if (jq_aNext.position().top > jq_aNext_before.position().top)
-					jq_aNext = jq_aNext_before;
-				else
-					continue;
-				break;
+		var	hash = window.location.hash;
+		function slideUp() {
+			jq_page.slideUp(250);
+			jq_aOld
+				.attr("href", jq_aOld.attr("hrefOld"))
+				.parent()
+					.removeClass("selected");
+		}
+
+		if (!hash) {
+			if (jq_aOld && jq_aOld[0]) {
+				slideUp();
 			}
-			if (!jq_aNext) {
-				setPage(jq_a);
-			} else {
-				jq_page.slideUp(250, function() {
-					setPage(jq_a);
-					jq_page
-						.hide()
-						.insertAfter(jq_aNext)
-						.slideDown(250);
-				});
+		} else {
+			var jq_a = $("[href='" + hash + "']");
+			if (jq_a.prop("pageData")) {
+				var	jq_aNext_before,
+					jq_aNext = jq_a;
+				for (;;) {
+					jq_aNext_before = jq_aNext;
+					jq_aNext = jq_aNext.next();
+					if (!jq_aNext[0])
+						jq_aNext = jq_aNext_before;
+					else if (jq_aNext[0].tagName !== "A")
+						jq_aNext = null;
+					else if (jq_aNext.position().top > jq_aNext_before.position().top)
+						jq_aNext = jq_aNext_before;
+					else
+						continue;
+					break;
+				}
+				if (jq_aOld && jq_a[0] === jq_aOld[0]) {
+					if (jq_a.attr("href") === "#") {
+						slideUp();
+					} else {
+						writeData(jq_a);
+						jq_page
+							.slideDown(250);
+					}
+				} else if (!jq_aNext) {
+					writeData(jq_a);
+					jq_page.slideDown(250);
+				} else {
+					jq_page.slideUp(250, function() {
+						writeData(jq_a);
+						jq_page
+							.hide()
+							.insertAfter(jq_aNext)
+							.slideDown(250);
+					});
+				}
 			}
 		}
 	};
