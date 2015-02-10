7c156b7041a6d9e10824da1c04b7edfac3842174
Mr21 <thomastortorini@gmail.com>
2015-02-10 14:08:43 +0100

    JS, projectsData + pageData

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ function lg(s) { console.log(s) }
 
 $(function() {
 
+	var	jq_page = $("<div>").addClass("page"),
+		jq_projectsContent = $(".rub.projects .content");
+
+	$.each(window.projectsData, function() {
+		$('<a href="#' + this.href + '" style="background-image: url(' + this.img + ');"></a>')
+			.appendTo(jq_projectsContent)
+			[0].pageData = this;
+	});
+
 	$(".rub .content").each(function() {
 		var	idTimeout,
 			jq_a = $(this).children("a");
@@ -17,5 +26,41 @@ $(function() {
 				}, 50);
 			});
 	});
+
+	function setPage(jq_a) {
+		lg(jq_a[0].pageData);
+	}
+
+	window.onhashchange = function() {
+		var jq_a = $("[href='" + window.location.hash + "']");
+		if (jq_a[0]) {
+			var	jq_aNext_before,
+				jq_aNext = jq_a;
+			for (;;) {
+				jq_aNext_before = jq_aNext;
+				jq_aNext = jq_aNext.next();
+				if (!jq_aNext[0])
+					jq_aNext = jq_aNext_before;
+				else if (jq_aNext[0].tagName !== "A")
+					jq_aNext = null;
+				else if (jq_aNext.position().top > jq_aNext_before.position().top)
+					jq_aNext = jq_aNext_before;
+				else
+					continue;
+				break;
+			}
+			if (!jq_aNext) {
+				setPage(jq_a);
+			} else {
+				jq_page.slideUp(250, function() {
+					setPage(jq_a);
+					jq_page
+						.insertAfter(jq_aNext)
+						.slideDown(250);
+				});
+			}
+		}
+	};
+	window.onhashchange();
 
 });
