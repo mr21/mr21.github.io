689323cf79ffd58c8665ed93fea0fdd2b2c360af
Mr21 <thomastortorini@gmail.com>
2015-03-01 01:23:47 +0100

    JS, socialLinks, escape to close

--- main.js
+++ main.js
@@ -88,47 +88,6 @@ $(function() {
 		return false;
 	});
 
-	// .social -----------------------------
-	(function () {
-		var	jq_social = $(".social"),
-			jq_links = $(".link > *", jq_social),
-			jq_oldLink = $(),
-			jq_close = $(".close", jq_social),
-			fn_close = function() {
-				jq_oldLink
-					.removeClass("open")
-					.parent()
-						.removeClass("selected")
-						.parent()
-							.removeClass("one-is-selected");
-				jq_oldLink[0]._jq_link.fadeOut(200);
-			};
-
-		jq_close.click(function() {
-			fn_close();
-			return false;
-		});
-
-		$("a[data]", jq_social)
-			.each(function(i) {
-				this._jq_link = jq_links.eq(i);
-			})
-			.click(function() {
-				var	jq_this = $(this);
-				if (!jq_this.hasClass("open")) {
-					jq_oldLink = jq_this;
-					jq_this
-						.addClass("open")
-						.parent()
-							.addClass("selected")
-							.parent()
-								.addClass("one-is-selected");
-					this._jq_link.fadeIn(500);
-				}
-				return false;
-			});
-	})();
-
 	// -------------------------------------
 
 	var	jq_aOld,
