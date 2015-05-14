1d32f21bbdae0b34ce7dbef35c94b70d946f91a5
Thomas Tortorini <thomastortorini@gmail.com>
2015-05-14 04:52:13 +0200

    JS, waiting, scrollTop animate

--- waiting.js
+++ waiting.js
@@ -1,5 +1,11 @@
 function waiting() {
 	$(window).load(function() {
+		var p = locationHash.data.p;
 		$(".waiting").addClass("fadeOut");
+		if (p) {
+			$(document.body).animate({
+				scrollTop: $("[name='"+p+"']").position().top
+			}, 400);
+		}
 	});
 }
