9f07dfe3d8c2060db5b9c082b926107243c74af6
Thomas Tortorini <thomastortorini@gmail.com>
2015-07-15 00:16:36 +0200

    JS, waiting, fallback if window.load never fire

--- waiting.js
+++ waiting.js
@@ -1,11 +1,23 @@
 function waiting() {
-	$(window).load(function() {
-		var p = locationHash.data.p;
-		$(".waiting").addClass("fadeOut");
-		if (p) {
-			$(document.body).animate({
-				scrollTop: $("[name='"+p+"']").position().top
-			}, 400);
+	var
+		pos,
+		faded = false,
+		jqWaiting = $(".waiting")
+	;
+
+	function fadeOut() {
+		if ( !faded ) {
+			faded = true;
+			jqWaiting.addClass( "fadeOut" );
+			pos = $( "[name='"+ locationHash.data.p +"']" ).position();
+			if ( pos ) {
+				$( "html, body" ).animate({
+					scrollTop: pos.top
+				}, 400);
+			}
 		}
-	});
+	}
+
+	setTimeout( fadeOut, 5000 );
+	$( window ).load( fadeOut );
 }
