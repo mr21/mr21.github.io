46f4993a1dcae1535c7e33c92ec80dacbdb59f67
Thomas Tortorini <thomastortorini@gmail.com>
2018-07-12 04:23:34 +0200

    remove everyting, just add a paragraphe with fancy fonts

--- design.js
+++
@@ -1,7 +0,0 @@
-function design() {
-	locationHash.watch({
-		light: function(l) {
-			document.body.className = l ? "light" : "";
-		}
-	});
-}
--- projectsSubtitles.js
+++
@@ -1,8 +0,0 @@
-function projectsSubtitles(dur) {
-	$("#data .subTitle").click(function() {
-		$(this)
-			.toggleClass("open")
-			.next().slideToggle(dur);
-		return false;
-	});
-}
